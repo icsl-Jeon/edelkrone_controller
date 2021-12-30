8f29b3e009a441db28efd4a2cf9ec7bfa825d3e9
B.S Felipe Jeon <junbs95@gmail.com>
2021-12-30 18:52:48 +0900

    calibration data was made.

--- extrinsic_edelkrone.py
+++ extrinsic_edelkrone.py
@@ -3,8 +3,9 @@ This script calibrate the extrinsic parameters.
 
 
 """
+import math
 
-
+import numpy as np
 import requests
 import json
 import time
@@ -40,7 +41,7 @@ def keyposeStoreNumeric(linkID, pan, tilt, slide, index, verbose=True):
     print(json.dumps(response_json, indent=2))
 
 # ---------------------------------------------------------------------------
-def keyposeMoveFixedDuration(linkID, duration, index, accel):
+def keyposeMoveFixedDuration(linkID, duration, index, accel, verbose = True):
 
   # WARNING
   # =======
@@ -65,7 +66,9 @@ def keyposeMoveFixedDuration(linkID, duration, index, accel):
   # ...
   response = requests.request("POST", url, headers=headers, json=payload)
   response_json = json.loads(response.text)
-  print(json.dumps(response_json, indent=2))
+
+  if verbose:
+      print(json.dumps(response_json, indent=2))
 
 # ---------------------------------------------------------------------------
 def bundleStatus(linkID, verbose=True):
@@ -86,27 +89,93 @@ def bundleStatus(linkID, verbose=True):
 
 
 if __name__ == "__main__":
-    zed = sl.Camera()
+
+    # 1. Opening Edelkrone
     aimIndex = 0
     linkID = "2065349D5853"
-    # startSlide = float(status["data"]["readings"]["slide"])
-    # startPan = float(status["data"]["readings"]["headPan"])
-    # startTilt = float(status["data"]["readings"]["headTilt"])
 
-    while True:
+    moveDuration_sec = 5
+    print("For {} sec, moving toward the reference slide,pan,tilt (0 cm,90 deg,0 deg)...".format(moveDuration_sec))
+    keyposeStoreNumeric(linkID, 90, 0, 0, aimIndex)
+    acceleration = 0.5
+    keyposeMoveFixedDuration(linkID, moveDuration_sec, aimIndex, acceleration)
+    time.sleep(moveDuration_sec + 2.0)
+    print("Done.")
+    pan = 90
+    tilt = 0
+    slide = 0
+
+    # Full range motion limits
+    panFullRange = 45.0  # in degrees
+    tiltFullRange = 5.0  # in degrees
+    slideFullRange = 10.0  # in centimeters
+    step = 10
+    panStep = panFullRange / step
+    tiltStep = tiltFullRange / step
+    slideStep = slideFullRange / step
+
+    # 2. After moving, turn zed camera
+    zed = sl.Camera()
+
+    init_parameters = sl.InitParameters()
+    init_parameters.coordinate_system = sl.COORDINATE_SYSTEM.RIGHT_HANDED_Z_UP_X_FWD # prefer ENU system.
+    init_parameters.camera_resolution = sl.RESOLUTION.HD1080
+    init_parameters.camera_fps = 30
+    init_parameters.coordinate_units = sl.UNIT.METER
+    err = zed.open(init_parameters)
+
+    positional_tracking_parameters = sl.PositionalTrackingParameters()
+    zed.enable_positional_tracking(positional_tracking_parameters)
+
+    runtime = sl.RuntimeParameters()
+    camera_pose = sl.Pose()
+    py_translation = sl.Translation()
+    pose_data = sl.Transform()
+
+    total_data = np.zeros((step,3+16))
+
+    for x in range(0, step):
+        # Get Edelkrone data
         status = bundleStatus(linkID, False)
         if status["result"] == 'ok':
-            startSlide = float(status["data"]["readings"]["slide"])
-            startPan = float(status["data"]["readings"]["headPan"])
-            startTilt = float(status["data"]["readings"]["headTilt"])
-
-            keyposeStoreNumeric(linkID, startPan, startTilt, startSlide, aimIndex, False)
+            # desired state stored
+            pan += panStep
+            tilt += tiltStep
+            slide += slideStep
+            keyposeStoreNumeric(linkID, pan, tilt, slide, aimIndex)
             time.sleep(0.250)
-            print("slide={:.3f} [cm] / pan={:.3f} [deg] / tilt={:.3f} [deg]".format(startSlide,startPan,startTilt))
+            # execute
+            keyposeMoveFixedDuration(linkID, 1.5, aimIndex, acceleration)
+            time.sleep(1)
+
+            # read state
+            status = bundleStatus(linkID, False)
+            curSlide = float(status["data"]["readings"]["slide"])
+            curPan = float(status["data"]["readings"]["headPan"])
+            curTilt = float(status["data"]["readings"]["headTilt"])
+
+            time.sleep(0.250)
+            print("slide={:.3f} [cm] / pan={:.3f} [deg] / tilt={:.3f} [deg]".format(curSlide, curPan, curTilt))
 
         else:
             print("connection problem !")
             time.sleep(0.250)
 
+        # ZED retrieving
+        text_translation = ""
+        text_rotation = ""
+        if zed.grab(runtime) == sl.ERROR_CODE.SUCCESS:
+            tracking_state = zed.get_position(camera_pose)
+            if tracking_state == sl.POSITIONAL_TRACKING_STATE.OK:
+                rotation = camera_pose.get_rotation_vector()
+                translation = camera_pose.get_translation(py_translation)
+                text_rotation = str((round(rotation[0], 2), round(rotation[1], 2), round(rotation[2], 2)))
+                text_translation = str((round(translation.get()[0], 2), round(translation.get()[1], 2), round(translation.get()[2], 2)))
+                pose_data = camera_pose.pose_data(sl.Transform())
+                print("zed translation (ENU coordinate): {} ".format(text_translation))
 
+        # Save data
+        total_data[x][0:3] = [curSlide*0.01, curPan*math.pi/180.0, curTilt*math.pi/180.0]
+        total_data[x][3:] = pose_data.m.flatten("C")
 
+    np.savetxt("calibration_result.txt",total_data)
